c58e2092a9015304697d3e8f1c4574389f0d14e0
Eric Chrobak <eric@ericchrobak.com>
2018-08-22 04:58:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -36,6 +36,12 @@ class Student
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+  end
+
+  def self.create()
+
   end
 
 end
